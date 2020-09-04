0c1077f21817caa1d83a1456e2afa6adb48dd614
Elton A Pering <elton_pering@adsomos.com>
2020-09-04 17:41:43 -0300

    criação classes e interfaces

---
+++ Entities/Entities/Produto.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Notifications;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Entities
+{
+    [Table("Product")]
+    public class Produto : Notifies
+    {
+        [Column("PRD_ID")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("PRD_NOME")]
+        [Display(Name = "NOME")]
+        public string Nome { get; set; }
+
+        [Column("PRD_VALOR")]
+        [Display(Name = "Valor")]
+        public decimal Valor { get; set; }
+
+        [Column("PRD_ESTADO")]
+        [Display(Name = "Estado")]
+        public bool Estado { get; set; }
+    }
+}
